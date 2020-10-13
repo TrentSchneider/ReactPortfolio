76cf3737a50f0b134ab9e03b36d224bb2bc9c26a
Trent Schneider <trentonschneider@gmail.com>
2020-10-13 15:39:55 -0400

    added Home page to pages

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <section className="bg-white homeBack h-100 TBBuffer">
+    <div className="col-sm-12 bg-white cardCenter h-100">
+      <div id="homeBackground" className="card noBord h-100"></div>
+    </div>
+  </section>
+  );
+}
+
+export default Home;
