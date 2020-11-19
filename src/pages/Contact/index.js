92b34ac7d2421567eee15cd12dc42851e784a9a6
Trent Schneider <trentonschneider@gmail.com>
2020-11-19 14:30:54 -0500

    made content adjustments to contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -8,12 +8,12 @@ function Contact() {
       <div className="container noMarg col-12 h-100">
         <div className="row h-100">
           <div className="col-sm-12 bg-white cardCenter">
-            <div className="card-body">
+            <div className="card-body h-100">
               <h4 className="portTitle lowBorder contactCardHead font-weight-bold">
                 Contact
               </h4>
 
-              <form>
+              {/* <form>
                 <div className="form-group">
                   <label for="userName" class="text-black-50">
                     Name
@@ -49,13 +49,25 @@ function Contact() {
                 <button type="submit" className="btn btn-dark">
                   Submit
                 </button>
-              </form>
-              <div className="row mb-3">
-                <div className="d-flex iconPosition col-sm-12 col-md-6">
-                  <a href="https://github.com/TrentSchneider">
+              </form> */}
+              <div className="row h-50">
+                <h3 className="text-center w-100 va mb-0">
+                  Have questions or want to get in touch? <br />
+                  Please contact me through any of the below links:
+                </h3>
+              </div>
+              <div className="row mb-3 h-50">
+                <div
+                  className="d-flex justify-content-center col-sm-12 va"
+                  //  col-md-6 iconPosition
+                >
+                  <a href="https://github.com/TrentSchneider" target="#">
                     <img src={githubIcon} alt="Github" className="contIcon" />
                   </a>
-                  <a href="https://www.linkedin.com/in/trentonschneider/">
+                  <a
+                    href="https://www.linkedin.com/in/trentonschneider/"
+                    target="#"
+                  >
                     <img
                       src={linkedInIcon}
                       alt="linkedIn"
@@ -66,12 +78,12 @@ function Contact() {
                     <img src={emailIcon} alt="Email" className="contIcon" />
                   </a>
                 </div>
-                <h4
+                {/* <h4
                   id="phone"
                   className="float-center font-weight-bold col-sm-12 col-md-6"
                 >
                   Phone: 404-218-9178
-                </h4>
+                </h4> */}
               </div>
             </div>
           </div>
