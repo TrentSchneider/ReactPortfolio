7881584d8a87396df3efe3e6e54ce025878bbc06
Trent Schneider <trentonschneider@gmail.com>
2020-11-19 13:09:29 -0500

    updated formatting

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,7 @@ function Nav() {
       id="navBar"
       className="d-flex flex-column flex-md-row align-items-center px-md-4 mb-3 bg-white"
     >
-      <h2 className="my-0 mr-md-auto font-weight-normal py-3 px-4 col-12 col-md-auto styleFont navh2 text-center">
+      <h2 className="wf my-0 mr-md-auto font-weight-normal py-3 px-4 col-12 col-md-auto styleFont navh2 text-center">
         Trent Schneider
       </h2>
       <nav className="my-2 my-md-0 mr-md-3">
