1cbb0097e3ce6f81441fd9caaccb3f47a25423a0
TrentSchneider <trentonschneider@gmail.com>
2021-03-05 21:04:55 -0500

    Update to index.js in About page
    Update the text flow and moved the anchor to within the text.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -15,21 +15,23 @@ function About() {
                 id="profPic"
                 className="card float-left noBord col-sm-12 col-md-5 col-lg-3"
               ></div>
-              <p className="float-left m-3 card p-3 col-sm-12 col-md-6 col-lg-8 cardBack">
-                My name is Trent Schneider.
-                <br />
-                In a past life, I worked in game development and IT. Now, I am
-                working towards a career in web development. <br />
-                I am thrilled by the prospect of being able to solve each new
-                project I can be a part of as I work out and solve what needs to
-                be done to fulfill the vision of each page. <br />
-                <br />
-                See my resume <span>
+              <div className="float-left m-3 card p-3 col-sm-12 col-md-6 col-lg-8 cardBack">
+                <p>
+                  My name is Trent Schneider. In a past life, I worked in game
+                  development and IT. Now, I am working towards a career in web
+                  development. I am thrilled by the prospect of being able to
+                  solve each new project I can be a part of as I work out and
+                  solve what needs to be done to fulfill the vision of each
+                  page.
+                </p>
+                <p>
+                  See my resume{" "}
                   <a href={Resume} target="_blank" rel="noopener noreferrer">
                     here
                   </a>
-                </span>
-              </p>
+                  .
+                </p>
+              </div>
             </div>
           </div>
         </section>
